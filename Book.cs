d562d90524b01d7d182c64e80e188261716e3d09
Dido <100509456+Dido2002@users.noreply.github.com>
2023-12-09 17:35:34 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksAdapter
+{
+    class Book : Library   //Adapter
+    {
+        private BookData data;
+
+        //Constructor 
+
+        public Book(string id) 
+            :base (id)  
+        {   
+            
+        }
+
+        public override void Display()
+        {
+            data = new BookData();
+
+            nameOfBook = data.GetName(BookId);
+            author = data.GetAuthor(BookId);
+            year = data.GetYear(BookId);
+            genre = data.GetGenre(BookId);
+
+
+            base.Display();
+            Console.WriteLine("Name: {0}", nameOfBook);
+            Console.WriteLine("Author : {0}", author);
+            Console.WriteLine("Year: {0}", year);
+            Console.WriteLine("Genre: {0}", genre);
+        }
+    }
+}
